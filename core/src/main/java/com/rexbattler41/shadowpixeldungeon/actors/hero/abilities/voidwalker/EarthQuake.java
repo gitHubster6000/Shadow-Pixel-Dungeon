e8e52d4420fb07760a086406f9183b0876af6a8d
Rex <117361823+gitHubster6000@users.noreply.github.com>
2023-03-21 10:42:19 +0100

    added code for EarthQuake

--- core/src/main/java/com/rexbattler41/shadowpixeldungeon/actors/hero/abilities/voidwalker/EarthQuake.java
+++ core/src/main/java/com/rexbattler41/shadowpixeldungeon/actors/hero/abilities/voidwalker/EarthQuake.java
@@ -1,15 +1,38 @@
 package com.rexbattler41.shadowpixeldungeon.actors.hero.abilities.voidwalker;
 
+import com.rexbattler41.shadowpixeldungeon.Dungeon;
+import com.rexbattler41.shadowpixeldungeon.actors.buffs.Buff;
+import com.rexbattler41.shadowpixeldungeon.actors.buffs.Levitation;
+import com.rexbattler41.shadowpixeldungeon.actors.buffs.Paralysis;
 import com.rexbattler41.shadowpixeldungeon.actors.hero.Hero;
 import com.rexbattler41.shadowpixeldungeon.actors.hero.Talent;
 import com.rexbattler41.shadowpixeldungeon.actors.hero.abilities.ArmorAbility;
+import com.rexbattler41.shadowpixeldungeon.actors.mobs.Mob;
 import com.rexbattler41.shadowpixeldungeon.items.armor.ClassArmor;
 import com.rexbattler41.shadowpixeldungeon.ui.HeroIcon;
 
 public class EarthQuake extends ArmorAbility {
+
+    {
+        baseChargeUse = 35f;
+    }
     @Override
     protected void activate(ClassArmor armor, Hero hero, Integer target) {
+        for (int i = 0; i < 10; i++) {
+            for (Mob mob : Dungeon.level.mobs.toArray( new Mob[0] )) {
+                if (Dungeon.level.heroFOV[mob.pos]) {
+                    //deals 10%HT, plus 0-90%HP based on scaling
+                    mob.damage(Math.round(mob.HT/10f + (mob.HP * 0.225f)), this);
+                    if (mob.isAlive()) {
+                        Buff.prolong(mob, Paralysis.class, Paralysis.DURATION);
+                    }
+                }
+            }
+            if (hero.buff(Levitation.class) == null){
 
+            }
+
+        }
     }
 
     @Override
